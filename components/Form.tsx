d942cfe403463502dc026a8fe75fb5cb5dd31b4b
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-04-25 13:35:24 +1000

    add comments page and function

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
 import usePosts from "@/hooks/usePosts";
+import usePost from "@/hooks/usePost";
 import useRegisterModal from "@/hooks/useRegisterModal";
 import Button from "./Button";
 import Avatar from "./Avatar";
@@ -19,6 +20,7 @@ const Form = ({ placeholder, isComment, postId }: FormProps) => {
   const loginModal = useLoginModal();
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -27,18 +29,21 @@ const Form = ({ placeholder, isComment, postId }: FormProps) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : "/api/posts";
+
+      await axios.post(url, { body });
 
       toast.success("Tweet Created!");
 
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
@@ -50,10 +55,11 @@ const Form = ({ placeholder, isComment, postId }: FormProps) => {
           <div className="w-full">
             <textarea
               disabled={isLoading}
+              value={body}
               onChange={(e) => setBody(e.target.value)}
               className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white"
               placeholder={placeholder}
-            ></textarea>
+            />
             <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
             <div className="mt-4 flex flex-row justify-end">
               <Button
